76364052d6ab2e140808cb904a0f24fa2e713da1
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2025-10-28 13:42:22 +0700

    feat: add CI

--- apps/backend-nestjs/src/products/products.controller.spec.ts
+++ apps/backend-nestjs/src/products/products.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -8,6 +9,12 @@ describe('ProductsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
